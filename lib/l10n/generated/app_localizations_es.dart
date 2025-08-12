69774b50738fa5501bf9e397365caffa8b968069
furkanuysal <60571747+furkanuysal@users.noreply.github.com>
2025-08-12 23:49:47 +0300

    feat: Add Spanish and German language support

---
+++ lib/l10n/generated/app_localizations_es.dart
@@ -0,0 +1,125 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'Linguess';
+
+  @override
+  String get selectCategory => 'Seleccionar categoría';
+
+  @override
+  String get selectLevel => 'Seleccionar nivel';
+
+  @override
+  String get dailyWord => 'Palabra diaria';
+
+  @override
+  String get settings => 'Configuración';
+
+  @override
+  String get category => 'Categoría';
+
+  @override
+  String get category_food => 'Comida';
+
+  @override
+  String get category_animal => 'Animales';
+
+  @override
+  String get category_vehicle => 'Vehículos';
+
+  @override
+  String get nextWord => 'Siguiente palabra';
+
+  @override
+  String get checkAnswer => 'Comprobar respuesta';
+
+  @override
+  String get letterHint => 'Pista';
+
+  @override
+  String get yourWord => 'Tu palabra';
+
+  @override
+  String get level => 'Nivel';
+
+  @override
+  String get translation => 'Traducción';
+
+  @override
+  String get close => 'Cerrar';
+
+  @override
+  String get login => 'Iniciar sesión';
+
+  @override
+  String get logout => 'Cerrar sesión';
+
+  @override
+  String get profile => 'Perfil';
+
+  @override
+  String get correctAnswer => 'Respuesta correcta';
+
+  @override
+  String get correctText => 'Correcto';
+
+  @override
+  String get registerButtonText => '¿No tienes cuenta? Regístrate';
+
+  @override
+  String get email => 'Correo electrónico';
+
+  @override
+  String get password => 'Contraseña';
+
+  @override
+  String get register => 'Registrarse';
+
+  @override
+  String get registerSuccess => '¡Registro exitoso! Ya puedes iniciar sesión.';
+
+  @override
+  String get invalidEmail => 'Correo electrónico no válido';
+
+  @override
+  String get passwordTooShort =>
+      'La contraseña debe tener al menos 6 caracteres';
+
+  @override
+  String get gold => 'Oro';
+
+  @override
+  String get learnedWordsText => 'Palabras aprendidas';
+
+  @override
+  String get learnedWordsEmpty => 'No hay palabras aprendidas aún';
+
+  @override
+  String get errorOccurred => 'Ocurrió un error';
+
+  @override
+  String get noDataToShow => 'No hay datos para mostrar';
+
+  @override
+  String get errorSignInFailed => 'Error al iniciar sesión';
+
+  @override
+  String get wordCount => 'Número de palabras';
+
+  @override
+  String get settingsLearnedWords => 'Repetir palabras aprendidas';
+
+  @override
+  String get settingsSoundEffects => 'Efectos de sonido';
+
+  @override
+  String get learnedCountText => 'Palabra aprendida';
+}
